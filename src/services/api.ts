fb5c74990fb5d6605f9136c2d6c5700f428c8d03
gustavonvp <gustavonvp@gmail.com>
2022-02-21 18:36:13 -0300

    API axios  Closes #2

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,14 @@
+/* eslint-disable prettier/prettier */
 import axios from 'axios';
 
-export const api = axios.create();
+export const api = axios.create({
+    baseURL: 'https://localhost:3000',
+    timeout: 1000,
+    headers: {'X-Custom-Header': 'foobar'},
+    withCredentials: false, // default
+    method: 'GET',// default,
+    decompress: true // default
+
+});
+
+
